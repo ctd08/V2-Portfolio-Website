1230b78fa1f7b3058314bf5f17df42f9fdc571de
Cristina <cristina.tutunariu@yahoo.com>
2026-09-22 13:22:20 +0200

    added router paths to timeline cards

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import UnderConstructionView from '@/views/UnderConstructionView.vue'
 import AboutView from '@/views/AboutView.vue'
+import CareerView from '@/views/CareerView.vue'
 
 const routes = [
   { path: '/',         component: HomeView },
   { path: '/about',    component: AboutView },
-  { path: '/career',   component: UnderConstructionView },
+  { path: '/career',   component: CareerView },
+  { path: '/career/tha-ti', component: UnderConstructionView },
+  { path: '/career/tha-mt', component: UnderConstructionView },
+  { path: '/career/fos', component: UnderConstructionView },
+  { path: '/career/uka-kora', component: UnderConstructionView },
+  { path: '/career/uka-mediha', component: UnderConstructionView },
+  { path: '/career/uka-rad', component: UnderConstructionView },
+  { path: '/career/igel', component: UnderConstructionView },
   { path: '/projects', component: UnderConstructionView },
   { path: '/projects/:id', component: UnderConstructionView },
   { path: '/blog',     component: UnderConstructionView },
